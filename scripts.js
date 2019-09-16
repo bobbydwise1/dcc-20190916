1cec87ff9f9e360fb9a9c1ed919e586cea095da7
Robert Lee <bobbydwise1@hotmail.com>
2019-09-16 14:32:23 -0700

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*
+This problem was asked by Square.
+
+Assume you have access to a function toss_biased() which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the bias of the coin.
+
+Write a function to simulate an unbiased coin toss.
+*/
+
+
+
+$(document).ready(function() {
+
+  $('#form1').submit(function(){
+    event.preventDefault()
+    input1 = $('#input-1').val()
+    input1 = input1.replace(/\'/g,'"')
+    input2 = JSON.parse(input1)
+    console.log(input1)
+    $('#output-1').text(spiral(input2))
+  })
+
+});
