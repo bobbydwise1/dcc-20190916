154bf2c2b6605bcbe91ecdc90a1ff6964c6a68be
Robert Lee <bobbydwise1@hotmail.com>
2019-09-19 17:18:13 -0700

    Add functions.

--- scripts.js
+++ scripts.js
@@ -6,13 +6,45 @@ Assume you have access to a function toss_biased() which returns 0 or 1 with a p
 Write a function to simulate an unbiased coin toss.
 */
 
+//supposed we ran the biased function, and then stored the results of 12 coin tosses in an array.  This function below will simulate it:
+
 const toss_biased = () => {
-  return Math.random()
+  return Math.round(Math.random())
 }
 
-const unbiased = () => {
-  return Math.round(toss_biased())
+const toss_biased_real_array = () => {
+  let output = [];
+  for ( i = 0; i < 12; i ++ ) {
+    output.push(toss_biased())
+  }
+  return output
 }
+
+const toss_biased_into_array = () => {
+  let results = [
+    [0,0,0,0,0,0, 1,1,1,1,1,1], //50%
+    [0,0,0,0,1,1, 1,1,1,1,1,1], //66%
+    [0,0,0,1,1,1, 1,1,1,1,1,1], //75%
+    [0,1,1,1,1,1, 1,1,1,1,1,1], //92%
+  ]
+  return results[Math.floor(Math.random()*4)]
+}
+
+const unbiased = (yourArray) => {
+  //Determine what is the distribution
+  let count0 = 0
+  let count1 = 0
+  for ( i = 0; i < yourArray.length ; i ++ ) {
+    if (yourArray[i] == 1) {
+      count1++
+    } else {
+      count0++
+    }
+  }
+  //find the ratio of 1's over 0's
+  let ratio = count1/count0
+}
+
 
 $(document).ready(function() {
 
